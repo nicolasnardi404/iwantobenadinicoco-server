9477314536713c8945d807f911fc8d5b46d59cd4
esquizo ciborgue <esquizociborgue@esquizos-MacBook-Pro.local>
2024-05-31 16:02:39 +0200

    adjustment on index.js

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ dotenv.config();
 const app = express();
 app.use(cors());
 
-// Create a MySQL connection pool
 const db = mysql.createPool({
     host: 'localhost',
     user: 'iwantbenicoco',
@@ -18,16 +17,13 @@ const db = mysql.createPool({
     database: 'poetry_maker'
 });
 
-// Initialize OpenAI
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
     dangerouslyAllowBrowser: true
 });
 
-// Route to fetch all poems
 app.get('/', async (req, res) => {
     try {
-        // Query to select all poems from the database
         const query = 'SELECT * FROM poems';
         db.query(query, (err, results) => {
             if (err) {
@@ -41,15 +37,47 @@ app.get('/', async (req, res) => {
     }
 });
 
-// New endpoint for generating a poem
+
 app.get('/generate-poem', async (req, res) => {
     try {
-        const messages = [{
+        // choosing a random topic to guide AI
+        const randomMessage = [{
             role: 'user',
-            content: 'Make a poem'
-        }];
+            content: "Write existential poems about been a machine"
+        },
+        {
+            role: 'user',
+            content: "Write poems about machines and love"
+        },
+        {
+            role: 'user',
+            content: "Write experimental poems about nonsense topics that make sense in the end"
+        },
+        {
+            role: 'user',
+            content: "Craft a poem contemplating the transformation into mushrooms or machines upon death"
+        },
+        {
+            role: 'user',
+            content: "Write a a poem reflecting society of the world"
+        }
+    ]
+
+        function getRandomMessage() {
+            const randomIndex = Math.floor(Math.random() * randomMessage.length);
+            return randomMessage[randomIndex];
+        }
+
+        const chooseMessage = getRandomMessage()
+        console.log(chooseMessage);
+
+        const messages = [{
+            role:"system",
+            content:"You are a poetry machine that makes poems max 20 lines"
+        }, chooseMessage
+            ];
         const response = await openai.chat.completions.create({
-            model: 'ft:gpt-3.5-turbo-0125:personal:my-experiment-4:9SP3ezOl',
+            model: 'ft:gpt-3.5-turbo-0125:personal:copynicv-3:9UtycjS0',
             messages: messages
         });
         const poemGenerated = response.choices[0].message.content;
