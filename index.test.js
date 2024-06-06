fadcbcccfe71facb0c1a10064d0f12d7b818a943
esquizo ciborgue <esquizociborgue@esquizos-MBP.homenet.telecomitalia.it>
2024-06-06 19:18:52 +0200

    create new test

--- index.test.js
+++ index.test.js
@@ -1,32 +1,23 @@
-// import OpenAI from "openai";
-
-// const openai = new OpenAI({
-//     apiKey: process.env.OPENAI_API_KEY,
-//     dangerouslyAllowBrowser: true
-// });
-
 it('GET API from http://localhost:8080/ should return 200', async()=>{
   const response = await fetch('http://localhost:8080');
   expect(response.status).toBe(200);
 })
 
-// // Mock the external service call
-// jest.mock('./index', () => ({
-//     generatePoem: jest.fn().mockReturnValue({
-//       poem: 'A beautiful day in the neighborhood.',
-//       message: 'Poem generated successfully.'
-//     }),
-//   }));
-  
-//   describe('Generate Poem Endpoint', () => {
-//     it('should generate a poem and insert it into the database', async () => {
-//       const response = await fetch('http://localhost:8080/generate-poem');
-//       const data = await response.json();
-  
-//       expect(data.poem).toBeDefined();
-//       expect(data.message).toContain('Poem inserted with ID:');
-//       // Assert that the mocked function was called
-//       expect(openai.generatePoem).toHaveBeenCalled();
-//     });
-//   });
-  
+  describe('Generate Poem Endpoint', () => {
+    it('should generate a poem and insert it into the database', async () => {
+      const response = await fetch('http://localhost:8080/generate-poem');
+      const data = await response.json();
+      jest.setTimeout(100000);
+      expect(data.poem).toBeDefined();
+      
+      expect(data.message).toContain('Poem inserted with ID:');
+      
+    });
+  });
+
+  it('GET API from http://localhost:8080/generate-poem should return 200', async()=>{
+  const response = await fetch('http://localhost:8080/generate-poem');
+  //set the time of operation to be longer than default to be able to receive back data
+  jest.setTimeout(100000);
+  expect(response.status).toBe(200);
+})
