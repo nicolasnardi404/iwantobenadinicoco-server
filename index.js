f02d0b2b51aae91458c3850d181d49d098e4fa81
esquizo ciborgue <esquizociborgue@esquizos-MBP.homenet.telecomitalia.it>
2024-07-03 11:19:10 +0200

    add cron to generate a poem everyday

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import mysql from 'mysql';
 import OpenAI from "openai";
 import dotenv from 'dotenv';
+import cron from 'node-cron';
+import { setTimeout } from 'timers/promises';
 
 dotenv.config(); 
 
@@ -36,45 +38,29 @@ app.get('/', async (req, res) => {
     }
 });
 
-
-app.get('/generate-poem', async (req, res) => {
+async function generatePoem() {
     try {
-        // choosing a random topic to guide AI
-        const randomMessage = [{
-            role: 'user',
-            content: "Write existential poems about been a machine"
-        },
-        {
-            role: 'user',
-            content: "Write poems about machines and love"
-        },
-        {
-            role: 'user',
-            content: "Write experimental poems about nonsense topics that make sense in the end"
-        },
-        {
-            role: 'user',
-            content: "Craft a poem contemplating the transformation into mushrooms or machines upon death"
-        },
-        {
-            role: 'user',
-            content: "Write a a poem reflecting society of the world"
-        }
-    ]
+        // Choosing a random topic to guide AI
+        const randomMessage = [
+            { role: 'user', content: "Write existential poems about being a machine" },
+            { role: 'user', content: "Write poems about machines and love" },
+            { role: 'user', content: "Write experimental poems about nonsense topics that make sense in the end" },
+            { role: 'user', content: "Craft a poem contemplating the transformation into mushrooms or machines upon death" },
+            { role: 'user', content: "Write a poem reflecting society of the world" }
+        ];
 
         function getRandomMessage() {
             const randomIndex = Math.floor(Math.random() * randomMessage.length);
             return randomMessage[randomIndex];
         }
 
-        const chooseMessage = getRandomMessage()
+        const chooseMessage = getRandomMessage();
         console.log(chooseMessage);
 
-        const messages = [{
-            role:"system",
-            content:"You are a poetry machine that makes poems max 20 lines"
-        }, chooseMessage
-            ];
+        const messages = [
+            { role: "system", content: "You are a poetry machine that makes poems max 20 lines" },
+            chooseMessage
+        ];
         const response = await openai.chat.completions.create({
             model: 'ft:gpt-3.5-turbo-0125:personal:copynicv-3:9UtycjS0',
             messages: messages
@@ -86,12 +72,55 @@ app.get('/generate-poem', async (req, res) => {
             if (err) {
                 throw err;
             }
-            res.json({ message: `Poem inserted with ID: ${results.insertId}`, poem: poemGenerated });
+            console.log(`Poem inserted with ID: ${results.insertId}`);
         });
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        console.error(error.message);
     }
+}
+
+async function generatePoemWithDelay() {
+
+    // Calculate a random delay within a 24-hour period
+    const randomDelaySeconds = Math.floor(Math.random() * 86400);
+    console.log(`Waiting for ${randomDelaySeconds} seconds...`);
+
+    // Wait for the calculated delay
+    await setTimeout(randomDelaySeconds * 1000); // Convert seconds to milliseconds
+
+     // Generate the poem   
+     await generatePoem();
+
+    console.log("Poem generation completed.");
+}
+
+//create a function if cron to make it runs everyday
+
+const generatePoemCronJob = cron.schedule('0 0 * * *', async () => {
+    console.log('Starting poem generation...');
+    await generatePoemWithDelay();
+}, {
+    scheduled: true,
+    timezone: 'Europe/Rome' 
 });
+
+// function to randomly choose if should write a second poem 
+const maybeGenerateASecondPoemCronJob = cron.schedule('0 0 * * *', async () => {
+
+    const randomNumber = Math.random();
+
+    if (randomNumber < 0.5) { // 50% chance
+        console.log('Generating poem...');
+        await generatePoemWithDelay();
+    } else {
+        console.log('Not generating second poem today.');
+    }
+
+})
+ 
+
+generatePoemCronJob.start();
+maybeGenerateASecondPoemCronJob.start();
 
 app.listen(8080, () => {
     console.log('Server listening on port 8080');
