430088614e64a24efebfccb11247a81a358001f3
esquizo ciborgue <esquizociborgue@esquizos-MBP.homenet.telecomitalia.it>
2024-07-08 13:04:29 +0200

    connecting db and server to vercel

--- api/index.js
+++ api/index.js
@@ -2,8 +2,6 @@ import express from "express";
 import cors from "cors";
 import OpenAI from "openai";
 import dotenv from "dotenv";
-import cron from "node-cron";
-import { setTimeout } from "timers/promises";
 import { PrismaClient } from "@prisma/client";
 
 dotenv.config();
@@ -17,18 +15,8 @@ const openai = new OpenAI({
   dangerouslyAllowBrowser: true,
 });
 
-app.get("/", async (req, res) => {
+async function generatePoem(req, res) {
   try {
-    const poems = await prisma.poetryMaker.findMany();
-    res.json(poems);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-async function generatePoem() {
-  try {
-    // Choosing a random topic to guide AI
     const randomMessage = [
       {
         role: "user",
@@ -45,7 +33,10 @@ async function generatePoem() {
         content:
           "Craft a poem contemplating the transformation into mushrooms or machines upon death",
       },
-      { role: "user", content: "Write a poem reflecting society of the world" },
+      {
+        role: "user",
+        content: "Write a poem reflecting society of the world",
+      },
     ];
 
     function getRandomMessage() {
@@ -63,73 +54,57 @@ async function generatePoem() {
       },
       chooseMessage,
     ];
+
     const response = await openai.chat.completions.create({
       model: "ft:gpt-3.5-turbo-0125:personal:copynicv-3:9UtycjS0",
       messages: messages,
     });
+
     const poemGenerated = response.choices[0].message.content;
+
+    const currentDate = new Date();
+    // Generate a random hour between 0 and 23
+    const randomHour = Math.floor(Math.random() * 24);
+    // Generate a random minute between 0 and 59
+    const randomMinute = Math.floor(Math.random() * 60);
+    // Generate a random second between 0 and 59
+    const randomSecond = Math.floor(Math.random() * 60);
+
+    // Create a new Date object with the random time
+    const randomTime = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      currentDate.getDate(),
+      randomHour,
+      randomMinute,
+      randomSecond
+    );
 
     const result = await prisma.poetryMaker.create({
       data: {
         poem: poemGenerated,
         prompt: chooseMessage.content,
-        date: new Date(),
+        date: randomTime,
       },
     });
 
     console.log(`Poem inserted with ID: ${result.id}`);
+    res.status(200).json(result);
   } catch (error) {
     console.error(error.message);
+    res.status(500).json({ error: error.message }); // Respond with an error message
   }
 }
 
-async function generatePoemWithDelay() {
-  // Calculate a random delay within a 24-hour period
-  const randomDelaySeconds = Math.floor(Math.random() * 86400);
-  console.log(`Waiting for ${randomDelaySeconds} seconds...`);
-
-  // Wait for the calculated delay
-  await setTimeout(randomDelaySeconds * 1000); // Convert seconds to milliseconds
-
-  // Generate the poem
-  await generatePoem();
-
-  console.log("Poem generation completed.");
-}
-
-//create a function if cron to make it runs everyday
-
-const generatePoemCronJob = cron.schedule(
-  "0 0 * * *",
-  async () => {
-    console.log("Starting poem generation...");
-    await generatePoemWithDelay();
-  },
-  {
-    scheduled: true,
-    timezone: "Europe/Rome",
-  }
-);
-
-// function to randomly choose if should write a second poem
-const maybeGenerateASecondPoemCronJob = cron.schedule("0 0 * * *", async () => {
-  const randomNumber = Math.random();
-
-  if (randomNumber < 0.5) {
-    // 50% chance
-    console.log("Generating poem...");
-    await generatePoemWithDelay();
-  } else {
-    console.log("Not generating second poem today.");
+app.get("/", async (req, res) => {
+  try {
+    const poems = await prisma.poetryMaker.findMany();
+    res.json(poems);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
-generatePoemCronJob.start();
-maybeGenerateASecondPoemCronJob.start();
+app.get("/generate-poem", generatePoem); // Directly use the generatePoem function as the handler
 
-//if you need to quickly generate a poem for testing app
-generatePoem();
-
-app.listen(8080, () => {
-  console.log("Server listening on port 8080");
-});
+export default app;
